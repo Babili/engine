2932fea7189baad73266cbcc0c94a26404da57ad
Thibault Poncelet <thibaultponcelet@gmail.com>
2017-07-31 17:10:59 +0200

    Add QA env

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -2,6 +2,6 @@ if !Rails.env.development? && !Rails.env.test? && !ENV["SENTRY_DSN"].nil?
   require "raven"
   Raven.configure do |config|
     config.dsn          = ENV["SENTRY_DSN"]
-    config.environments = %w[ staging production ]
+    config.environments = %w[ qa staging production ]
   end
 end
